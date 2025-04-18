18d4866a1ca00de60aea858309cb30fc197d1892
Daniel <kdalmirante@gmail.com>
2025-04-18 19:06:37 +0800

    Add a personal message to the HTML header

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ include 'includes/connection.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test</title>
     <link rel="stylesheet" href="styles.css">
+    <h2>This time sarili ko muna pati ikaw iloveyou hehe</h2>
     <script src="script.js"></script>
 </head>
 
