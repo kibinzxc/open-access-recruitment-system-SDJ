08cc57677b9cfb280a252a05563fc8550faebe04
Daniel <kdalmirante@gmail.com>
2025-04-19 19:28:30 +0800

    Test

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <?php
-include 'includes/connection.php';
+include 'includes/db-connection.php';
 ?>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title>Sweet Dream Job</title>
     <link rel="stylesheet" href="styles.css">
-    <h2>This time sarili ko muna pati ikaw iloveyou hehe</h2>
+    <img src="assets/images/test.jpg" alt="Sweet Dream Job Logo" class="logo">
     <script src="script.js"></script>
 </head>
 
