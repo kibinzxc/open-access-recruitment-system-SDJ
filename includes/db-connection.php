bf2b93fce42ca8c833e7674a7679f3c45358890f
Daniel <kdalmirante@gmail.com>
2025-04-19 19:31:21 +0800

    Comment out successful connection message in db-connection.php

--- includes/db-connection.php
+++ includes/db-connection.php
@@ -8,4 +8,4 @@ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully!";
+// echo "Connected successfully!";
