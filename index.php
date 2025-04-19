456f6335ecd75b9273b03568f742f10316a68550
Daniel <kdalmirante@gmail.com>
2025-04-19 19:33:56 +0800

    test gitignore

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include 'includes/db-connection.php';
     <title>Sweet Dream Job</title>
     <link rel="stylesheet" href="styles.css">
     <center>
-        <img src="assets/images/test.jpg" alt="Sweet Dream Job Logo" class="logo" width="500" height="auto">
+        <img src="assets/images/test.jpg" alt="Sweet Dream Job Logo" class="logo" style="width: 100%; height: auto;">
     </center>
     <script src="script.js"></script>
 </head>
