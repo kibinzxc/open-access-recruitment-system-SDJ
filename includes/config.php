d7167353472b03aab75d8deb2b645e32b4e8fb84
Daniel <kdalmirante@gmail.com>
2025-04-19 19:37:49 +0800

    Refactor database connection settings for clarity and consistency

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,15 @@
 <?php
 // Database connection settings
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'u238690332_sdj');
-// define('DB_PASS', 'Kevin0405!');
-// define('DB_NAME', 'u238690332_sdj');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u238690332_sdj');
+define('DB_PASS', 'Kevin0405!');
+define('DB_NAME', 'u238690332_sdj');
 
 // Connect to the database locally (XAMPMP)
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'sdj_db');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'sdj_db');
 
 // Error reporting settings
 error_reporting(E_ALL); // Show all errors for debugging
