852cfe99ad16e090b60bfda52910ad5638eaf43b
Daniel <kdalmirante@gmail.com>
2025-04-21 12:41:25 +0800

    Update Open Graph and Twitter card image paths to use the new icon

--- home.php
+++ home.php
@@ -14,12 +14,12 @@ include 'includes/db-connection.php';
     <meta property="og:url" content="https://sweetdreamjob.com/" />
     <meta property="og:title" content="Sweet Dream Job Website" />
     <meta property="og:description" content="Sweet Dream Job - Your dream job awaits!" />
-    <meta property="og:image" content="https://replug.io/images/example.png" />
+    <meta property="og:image" content="assets/images/sdj-icon.png" />
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://sweetdreamjob.com/" />
     <meta property="twitter:title" content="Sweet Dream Job Website" />
     <meta property="twitter:description" content="Sweet Dream Job - Your dream job awaits!" />
-    <meta property="twitter:image" content="https://replug.io/images/example.png" />
+    <meta property="twitter:image" content="assets/images/sdj-icon.png" />
     <link rel="icon" href="assets/images/icon.svg" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/index.css">
     <title>Sweet Dream Job</title>
