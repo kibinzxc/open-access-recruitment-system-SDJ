6c512ef146420f8187b2a501a3ddb4b04503ae1a
Daniel <kdalmirante@gmail.com>
2025-04-21 12:45:34 +0800

    add SEO tags

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php 
 header("Location: home.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Sweet Dream Job - Your dream job awaits!">
+    <meta name="keywords" content="job, career, dream job, employment, opportunities">
+    <meta name="title" content="Sweet Dream Job Website" />
+    <meta property="og:type content=" website" />
+    <meta property="og:url" content="https://sweetdreamjob.com/" />
+    <meta property="og:title" content="Sweet Dream Job Website" />
+    <meta property="og:description" content="Sweet Dream Job - Your dream job awaits!" />
+    <meta property="og:image" content="assets/images/sdj-icon.png" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://sweetdreamjob.com/" />
+    <meta property="twitter:title" content="Sweet Dream Job Website" />
+    <meta property="twitter:description" content="Sweet Dream Job - Your dream job awaits!" />
+    <meta property="twitter:image" content="assets/images/sdj-icon.png" />
+    <link rel="icon" href="assets/images/icon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <title>Sweet Dream Job</title>
+    <!-- <link rel="stylesheet" href="styles.css">
+    <script src="script.js"></script> -->
+</head>
