3d1e2c3b6746b20bb73950c47c831c1fd765bf56
Daniel <kdalmirante@gmail.com>
2025-04-26 12:16:49 +0800

    Test fix on dreamy-stars index

--- includes/dreamy-stars.php
+++ includes/dreamy-stars.php
@@ -2,21 +2,12 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Evenly Spaced Falling Stars</title>
-    <style>
-    body {
-        margin: 0;
-        padding: 0;
-        height: 80vh;
-        overflow: hidden;
-    }
 
+    <style>
     .stars-container {
         position: absolute;
         width: 100%;
-        height: 100%;
+        height: 80vh;
         top: 0;
         left: 0;
         pointer-events: none;
