fff898615e9a2077ef2c911b4b9a966477dad259
Daniel <kdalmirante@gmail.com>
2025-05-02 07:25:27 +0800

    Fix HTML structure and enhance CSS for improved layout visibility on job and about us pages

--- about-us.php
+++ about-us.php
@@ -65,7 +65,7 @@ include 'includes/db-connection.php';
             with <span class="bold-text">opportunities</span> that truly fit you</p>
         <p>Take the first step toward the <span class="bold-text">career</span> you've always wanted!</p>
         <a class="submit-cv-button" href="jobs.php">
-            Submit your CV now<a>
+            Submit your CV now</a>
     </div>
 
 </body>
