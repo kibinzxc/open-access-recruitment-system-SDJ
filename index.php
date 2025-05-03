fec493f4b56e7d7e72a0227b649d28bb95dfa39a
Daniel <kdalmirante@gmail.com>
2025-05-04 07:51:03 +0800

    Config files

--- index.php
+++ index.php
@@ -2,29 +2,18 @@
 <html lang="en">
 
 <head>
-    <!-- HTML Meta Tags -->
-    <title>Sweet Dream Job</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Sweet Dream Job - Your dream job awaits!">
-
-    <!-- Facebook Meta Tags -->
-    <meta property="og:url" content="https://sweetdreamjob.com">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="Sweet Dream Job">
-    <meta property="og:description" content="Sweet Dream Job - Your dream job awaits!">
-    <meta property="og:image"
-        content="https://opengraph.b-cdn.net/production/images/11e7952b-30a8-49ee-9143-6a1de0f6d534.png?token=zZjfmBSD0D69hPysZn2gK5szwJhUbYaTf3eDdB8y5pg&height=262&width=424&expires=33281211347">
-
-    <!-- Twitter Meta Tags -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta property="twitter:domain" content="sweetdreamjob.com">
-    <meta property="twitter:url" content="https://sweetdreamjob.com">
-    <meta name="twitter:title" content="Sweet Dream Job Website">
-    <meta name="twitter:description" content="Sweet Dream Job - Your dream job awaits!">
-    <meta name="twitter:image"
-        content="https://opengraph.b-cdn.net/production/images/11e7952b-30a8-49ee-9143-6a1de0f6d534.png?token=zZjfmBSD0D69hPysZn2gK5szwJhUbYaTf3eDdB8y5pg&height=262&width=424&expires=33281211347">
-
+    <meta name="keywords" content="job, career, dream job, employment, opportunities">
+    <link rel="icon" href="../assets/images/icon.svg" type="image/x-icon">
+    <title>Admin | Sweet Dream Job</title>
 </head>
 
-<?php 
-header("Location: home.php");
-?>
+<body>
+    <h1>Hello There!</h1>
+    <p>Welcome to the admin panel. Here you can manage your job listings and applications.</p>
+    <img src="pages/assets/images/icon.svg" alt="">
+</body>
+
+</html>
---
+++ pages/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- HTML Meta Tags -->
+    <title>Sweet Dream Job</title>
+    <meta name="description" content="Sweet Dream Job - Your dream job awaits!">
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="https://sweetdreamjob.com">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Sweet Dream Job">
+    <meta property="og:description" content="Sweet Dream Job - Your dream job awaits!">
+    <meta property="og:image"
+        content="https://opengraph.b-cdn.net/production/images/11e7952b-30a8-49ee-9143-6a1de0f6d534.png?token=zZjfmBSD0D69hPysZn2gK5szwJhUbYaTf3eDdB8y5pg&height=262&width=424&expires=33281211347">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="sweetdreamjob.com">
+    <meta property="twitter:url" content="https://sweetdreamjob.com">
+    <meta name="twitter:title" content="Sweet Dream Job Website">
+    <meta name="twitter:description" content="Sweet Dream Job - Your dream job awaits!">
+    <meta name="twitter:image"
+        content="https://opengraph.b-cdn.net/production/images/11e7952b-30a8-49ee-9143-6a1de0f6d534.png?token=zZjfmBSD0D69hPysZn2gK5szwJhUbYaTf3eDdB8y5pg&height=262&width=424&expires=33281211347">
+
+</head>
+
+<?php 
+header("Location: home.php");
+?>
