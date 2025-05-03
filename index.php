6b619c713f5d89f30f35df650f44113c525bc757
Daniel <kdalmirante@gmail.com>
2025-05-04 07:59:22 +0800

    Fix icon path in index.php and remove unused test.php file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Sweet Dream Job - Your dream job awaits!">
     <meta name="keywords" content="job, career, dream job, employment, opportunities">
-    <link rel="icon" href="../assets/images/icon.svg" type="image/x-icon">
+    <link rel="icon" href="pages/assets/images/icon.svg" type="image/x-icon">
     <title>Admin | Sweet Dream Job</title>
 </head>
 
