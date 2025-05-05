58a4d1366840c7f26dfde1dceb96c25b929746fe
Daniel <kdalmirante@gmail.com>
2025-05-05 08:53:38 +0800

    Add login page and styles; update icon paths in CSS files

--- index.php
+++ index.php
@@ -8,12 +8,28 @@
     <meta name="keywords" content="job, career, dream job, employment, opportunities">
     <link rel="icon" href="pages/assets/images/icon.svg" type="image/x-icon">
     <title>Admin | Sweet Dream Job</title>
+    <link rel="stylesheet" href="pages/admin/styles/login.css">
 </head>
+<?php
+include('pages/includes/dreamy-stars2.php');
+?>
 
 <body>
-    <h1>Hello There!</h1>
-    <p>Welcome to the admin panel. Here you can manage your job listings and applications.</p>
-    <img src="pages/assets/images/icon.svg" alt="">
+    <div class="login-container">
+        <img src="pages/assets/images/sdj-icon.png" alt="">
+        <form action="pages/assets/php/admin/login.php" method="POST" class="login-form">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required>
+
+            <a class="forgot-password" href="#">Forgot Password?</a>
+            <button type="submit">Sign in</button>
+        </form>
+
+    </div>
+
 </body>
 
 </html>
