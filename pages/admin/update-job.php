3c15cc8616ebfdfd6ccb0e9cb26dbc9ebd62b1a9
Daniel <kdalmirante@gmail.com>
2025-05-15 18:36:17 +0800

    Refactor update-job.php to capitalize input fields for title, country, and availability; enhance responsibilities and qualifications processing.

--- pages/admin/update-job.php
+++ pages/admin/update-job.php
@@ -1,17 +1,24 @@
 <?php
 include("../includes/db-connection.php");
 
+function capitalizeWordsArray($array)
+{
+    return array_map(function ($item) {
+        return ucwords(strtolower($item));
+    }, $array);
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = intval($_POST['id']);
-    $title = mysqli_real_escape_string($conn, $_POST['title']);
-    $country = mysqli_real_escape_string($conn, $_POST['country']);
-    $availability = mysqli_real_escape_string($conn, $_POST['availability']);
+    $title = capitalizeWordsArray([mysqli_real_escape_string($conn, $_POST['title'])])[0];
+    $country = capitalizeWordsArray([mysqli_real_escape_string($conn, $_POST['country'])])[0];
+    $availability = capitalizeWordsArray([mysqli_real_escape_string($conn, $_POST['availability'])])[0];
     $description = mysqli_real_escape_string($conn, $_POST['description']);
     $oldImage = $_POST['old_image'] ?? '';
 
     // Decode JSON arrays
-    $responsibilities = json_decode($_POST['responsibilities'], true);
-    $qualifications = json_decode($_POST['qualifications'], true);
+    $responsibilities = capitalizeWordsArray(json_decode($_POST['responsibilities'], true));
+    $qualifications = capitalizeWordsArray(json_decode($_POST['qualifications'], true));
 
     $responsibilitiesJson = json_encode(["responsibilities" => $responsibilities]);
     $qualificationsJson = json_encode(["qualification" => $qualifications]);
